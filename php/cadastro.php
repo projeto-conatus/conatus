3183371301a69954e00eecdb34a3e14ec79b700a
deux-l <deuxl@outlook.com>
2020-11-04 14:33:21 -0300

    FormScriptJsBootstrap

--- php/cadastro.php
+++ php/cadastro.php
@@ -35,112 +35,237 @@
 
       </header>
 
-    <div class="container">
-        <form>
-            <div class="col-md-6 offset-md-3">
-
-            <center><h3>Cadastre-se <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-clipboard-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <center><h3>Cadastre-se <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-clipboard-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
   <path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3zM8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"/>
 </svg></h3></center>
-            
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="nome">Nome</label>
-                        <input type="text" class="form-control" id="nome" placeholder="">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="sobrenome">Sobrenome</label>
-                        <input type="text" class="form-control" id="sobrenome" placeholder="">
-                    </div>
-                    <div class="form-group col-md-7">
-                        <label for="CPF">CPF</label>
-                        <input type="text" class="form-control" id="CPF" placeholder="">
-                    </div>
-                    <div class="form-group col-md-5">
-                        <label for="data_Nascimento">Data nascimento</label>
-                        <input type="text" class="form-control" id="data_Nascimento" placeholder="">
-                    </div>
-                </div>
 
-                <hr>
 
-                <div class="form-row">
-                    <div class="form-group col-md-9">
-                        <label for="endereco">Endereço</label>
-                        <input type="text" class="form-control" id="endereco" placeholder="">
-                    </div>
-                    <div class="form-group col-md-3">
-                        <label for="numero">Numero</label>
-                        <input type="text" class="form-control" id="numero">
-                    </div>
-                    <div class="form-group col-md-7">
-                        <label for="complemento_Endereco">Complemento</label>
-                        <input type="text" class="form-control" id="complemento_Endereco" placeholder="">
-                    </div>
-                    <div class="form-group col-md-5">
-                        <label for="bairro">Bairro</label>
-                        <input type="text" class="form-control" id="bairro">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="cidade">Cidade</label>
-                        <input type="text" class="form-control" id="cidade">
-                    </div>
-                    <div class="form-group col-md-2">
-                        <label for="uf">UF</label>
-                        <input type="text" class="form-control" id="uf">
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="cep">CEP</label>
-                        <input type="text" class="form-control" id="cep">
-                    </div>
-                </div>
+<div class="container">
 
-                <hr>
 
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="escolaridade">Escolaridade</label>
-                        <input type="text" class="form-control" id="escolaridade" placeholder="">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="tipo_De_Escola">Tipo de Escola</label>
-                        <input type="text" class="form-control" id="tipo_De_Escola" placeholder="">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="grupo_Etnico_Racial"> Grupo Étnico Racial:</label>
-                        <input type="text" class="form-control" id="grupo_Etnico_Racial" placeholder="">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="genero">Gênero</label>
-                        <input type="text" class="form-control" id="genero" placeholder="">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="portador_Deficiencia"> Deficiente fisico?</label>
-                        <input type="text" class="form-control" id="portador_Deficiencia" placeholder="">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="tipo_Deficiencia">Tipo de Deficiência:</label>
-                        <input type="text" class="form-control" id="tipo_Deficiencia" placeholder="">
-                    </div>
-                </div>
+<div class="col-md-6 offset-md-3">
 
-                <hr>
+  <form class="needs-validation" novalidate>
 
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="text" class="form-control" id="inputEmail4" placeholder="email@exemplo.com">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Senha</label>
-                        <input type="text" class="form-control" id="inputPassword4" placeholder="Senha">
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Entrar</button>
-            </div>
-        </form>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="validationCustom01">Nome</label>
+        <input type="text" class="form-control" id="validationCustom01" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="validationCustom02">Sobrenome</label>
+        <input type="text" class="form-control" id="validationCustom02" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-7">
+        <label for="validationCustom03">CPF</label>
+        <input type="text" class="form-control" id="validationCustom03" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-5">
+        <label for="validationCustom04">Data de Nascimento</label>
+        <input type="text" class="form-control" id="validationCustom04" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
     </div>
+
+    <hr>
+
+    <div class="form-row">
+      <div class="form-group col-md-9">
+        <label for="validationCustom05">Endereço</label>
+        <input type="text" class="form-control" id="validationCustom05" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-3">
+        <label for="validationCustom06">Numero</label>
+        <input type="text" class="form-control" id="validationCustom06" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-7">
+        <label for="validationCustom07">Complemento</label>
+        <input type="text" class="form-control" id="validationCustom07" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-5">
+        <label for="validationCustom08">Bairro</label>
+        <input type="text" class="form-control" id="validationCustom08" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="validationCustom09">Cidade</label>
+        <input type="text" class="form-control" id="validationCustom09" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-2">
+        <label for="validationCustom10">UF</label>
+        <input type="text" class="form-control" id="validationCustom10" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-4">
+        <label for="validationCustom11">CEP</label>
+        <input type="text" class="form-control" id="validationCustom11" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="validationCustom12">Escolaridade</label>
+        <input type="text" class="form-control" id="validationCustom12" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="validationCustom13">Tipo de Escola</label>
+        <input type="text" class="form-control" id="validationCustom13" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="validationCustom14">Grupo Étnico Racial:</label>
+        <input type="text" class="form-control" id="validationCustom14" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="validationCustom15">Gênero</label>
+        <input type="text" class="form-control" id="validationCustom15" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="validationCustom16">Deficiente fisico?</label>
+        <input type="text" class="form-control" id="validationCustom16" placeholder="Sim / Não" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+
+      <div class="form-group col-md-6">
+        <label for="validationCustom17">Tipo de Deficiência:</label>
+        <input type="text" class="form-control" id="validationCustom17" placeholder="" value="" required>
+        <div class="invalid-feedback">
+          <p style="color: white;">Favor preencher este campo
+          <p>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+    <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="validationCustom18">Email</label>
+      <input type="text" class="form-control" id="validationCustom18" placeholder="email@exemplo.com" value="" required>
+      <div class="invalid-feedback">
+        <p style="color: white;">Favor preencher este campo
+        <p>
+      </div>
+    </div>
+
+    <div class="form-group col-md-6">
+      <label for="validationCustom19">Senha</label>
+      <input type="text" class="form-control" id="validationCustom19" placeholder="Senha" value="" required>
+      <div class="invalid-feedback">
+        <p style="color: white;">Favor preencher este campo
+        <p>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+    <button class="btn btn-primary btn-lg btn-block" type="submit">Cadastrar</button>
 </div>
+</div>
+</form>
+
+<center><footer style="color: white; padding: 50px;">Conatus-Squad09 &copy; RecodePro</footer><center>
+
+<!--ScriptJsBootstrap-->
+<script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function () {
+          'use strict';
+          window.addEventListener('load', function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function (form) {
+              form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+              }, false);
+            });
+          }, false);
+        })();
+      </script>
+
+<!--ScriptJsBootstrap-->
+</div>
+</div>
+
 </body>
+
 </html>
