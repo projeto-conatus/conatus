18670a44359cccb92388579c5c2592f969b93d90
Marcos Antonio <marcos-rj@outlook.com>
2020-11-05 19:04:54 -0300

    trabalhando no front end, e organizaçao de arquivos

--- php/cadastro.php
+++ php/cadastro.php
@@ -5,9 +5,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Conatus</title>
-  
-  <link rel="stylesheet" href="../css/cadastro.css"> 
-  <link rel="shortcut icon" href="../images/favlogo-blue.png" type="image/x-icon">
+  <!-- <link rel="stylesheet" href="../css/feed.css"> -->
+  <link rel="stylesheet" href="../css/cadastro.css">
+
+  <link rel="shortcut icon" href="../images/logo.svg" type="image/x-icon">
   
 
   <!--BOOTSTRAP-->
@@ -20,34 +21,26 @@
 <body>
  
 
-    <header>
+  <header>
 
-      <div class="headerContainer">
+    <div class="headerContainer">
 
-        <div class="logoContainer">
-          <img src="../images/logo.svg" alt="Logo da Conatus">
-        </div>
-
-        <nav class="navBar">
-            <a class="botaoVoltar"href="#"><img src="../images/arrow-back.svg" alt="">Voltar</a>
-        </nav>
+      <div class="logoContainer">
+        <img src="../images/logo.svg" alt="Logo da Conatus">
+        <span>Conatus</span>
       </div>
 
-    </header>
+      <nav class="navBar">
+        <a href="#" class="botaoInicio text-white">Início</a>
+        <div class="iconeVoltar">
+          <a href="#"><img src="../images/arrow-back.svg" alt="botão para voltar a página inicial"></a>
+        </div>
 
-    
-      <h3 class="titulocadastro">Cadastre-se <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-clipboard-plus" fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd"
-            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-          <path fill-rule="evenodd"
-            d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3zM8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z" />
-        </svg></h3>
-    
+  </header>
 
+      <h3 class="tituloCadastro">Cadastre-se</h3>
 
     <div class="container">
-
 
       <div class="col-md-6 offset-md-3">
 
@@ -56,7 +49,7 @@
           <div class="form-row">
             <div class="form-group col-md-6">
               <label for="validationCustom01">Nome</label>
-              <input type="text" class="form-control" id="validationCustom01" name="" placeholder="" required>
+              <input type="text" class="form-control" id="validationCustom01" name="" placeholder="Digite seu nome" required>
               <div class="invalid-feedback">
                 <p style="color: white;">Favor preencher este campo
                 <p>
@@ -65,7 +58,7 @@
 
             <div class="form-group col-md-6">
               <label for="validationCustom02">Sobrenome</label>
-              <input type="text" class="form-control" id="validationCustom02" name="" placeholder="" required>
+              <input type="text" class="form-control" id="validationCustom02" name="" placeholder="Digite seu sobrenome" required>
               <div class="invalid-feedback">
                 <p style="color: white;">Favor preencher este campo
                 <p>
@@ -74,7 +67,7 @@
 
             <div class="form-group col-md-7">
               <label for="validationCustom03">CPF</label>
-              <input type="text" class="form-control" id="validationCustom03" name="" placeholder="" required>
+              <input type="text" class="form-control" id="validationCustom03" name="" placeholder="Digite seu CPF" required>
               <div class="invalid-feedback">
                 <p style="color: white;">Favor preencher este campo
                 <p>
@@ -83,7 +76,7 @@
 
             <div class="form-group col-md-5">
               <label for="validationCustom04">Data de Nascimento</label>
-              <input type="text" class="form-control" id="validationCustom04" name="" placeholder="" required>
+              <input type="text" class="form-control" id="validationCustom04" name="" placeholder="Digite sua data de nascimento" required>
               <div class="invalid-feedback">
                 <p style="color: white;">Favor preencher este campo
                 <p>
@@ -240,7 +233,7 @@
           </div>
 
           <hr>
-          <button class="btn btn-primary btn-lg btn-block" type="submit">Cadastrar</button>
+          <button class="btn btn-primary btn-lg btn-block enviar" type="submit">Cadastrar</button>
       </div>
     </div>
     </form>
@@ -267,23 +260,6 @@
           })();
         </script>
         <!--ScriptJsBootstrap-->
-  
-        <footer>
-      <div class="footerContainer">
-        <img src="../images/logoC.svg" alt="logomarca da Conatus" class="logoFooter">
-
-        <p>Projeto Conatus &reg; 2020</p>
-
-        <div class="redesSociais">
-          <h4>Redes sociais</h4>
-          <div class="icones">
-            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-          </div>
-        </ul> 
-      </div>
-
-    </footer>
 
 </body>
 
