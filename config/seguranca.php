21038a2745312a0d6848815cbe8b32788d86221f
nathaliadv <nathaliadantasv@gmail.com>
2021-01-17 18:08:10 -0300

    Correção na mensagem de segurança

--- config/seguranca.php
+++ config/seguranca.php
@@ -1,14 +1,14 @@
 <?php
 function seguranca_usuario(){
     if ((empty($_SESSION['idUsuario']))) {
-        $_SESSION['loginErro'] = "Você tentou entrar em uma àrea restrita, favor efetuar login:";
+        $_SESSION['loginErro'] = "Você tentou entrar em uma área restrita, favor efetuar login:";
         header("Location:../views/login.php");    
         }
 }
 
 function seguranca_adm(){
     if (((empty($_SESSION['idColaborador'])))) {
-        $_SESSION['loginErro'] = "Você tentou entrar em uma àrea restrita, favor efetuar login:";
+        $_SESSION['loginErro'] = "Você tentou entrar em uma área restrita, favor efetuar login:";
         header("Location:../views/loginAdm.php");
     } 
 }
